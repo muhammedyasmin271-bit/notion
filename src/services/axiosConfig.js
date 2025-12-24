998ba8879b6b0b5c1cf40f744669ce3b23fb5c44
muhammedyasmin271-bit <muhammedyasmin271@gmail.com>
2025-12-24 17:13:21 +0300

    ffff10

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-    process.env.REACT_APP_API_URL ||
+ "https://notion-l9ti.onrender.com/api" ||
     (typeof window !== 'undefined' && window.location.hostname !== 'localhost'
         ? '/api'
         : 'https://notion-l9ti.onrender.com/api');
